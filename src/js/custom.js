06cd3a31942f3f591c3d3a515c7894f311f96e51
artlenk.ru@gmail.com <df5ukoi7ht>
2017-07-02 16:38:07 +0300

    Created about-web-site.html page

--- src/js/custom.js
+++ src/js/custom.js
@@ -25,4 +25,19 @@ $(document).ready(function () {
             });
         });
     });
+
+
+    $( function() {
+        $( "#slider-range" ).slider({
+            range: true,
+            min: 1500,
+            max: 25000,
+            values: [ 1500, 12500 ],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    } );
 });
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,4 +1,7 @@
 // = lib/smoothscroll.js
+
 //= lib/select2.min.js
 
 //= lib/jquery.mCustomScrollbar.min.js
+
+//= lib/jquery-ui.min.js
